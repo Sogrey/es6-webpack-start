cbed72f85b1aa3d160cee7ac63e4fc10d13e33b0
sogrey <suzhaoheng2006@163.com>
2020-01-11 12:24:56 +0800

    deploy master

--- build/bundle.js
+++ build/bundle.js
@@ -94,7 +94,7 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\n\n[1, 2, 3].map(function (item) {\n  return console.log(item);\n});\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("\n\n[1, 2, 3, 4].map(function (item) {\n  return console.log(item);\n});\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
