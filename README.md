1a44790126625a5af02586fc4232d500cdaf9ddf
sogrey <suzhaoheng2006@163.com>
2020-01-11 12:13:29 +0800

    deploy master

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # es6-webpack-start
 ECMAScript 6 入门
+
+## 参考
+
+- https://blog.csdn.net/qq_34117170/article/details/86623159
+- https://blog.csdn.net/m0_38134431/article/details/82666435
+- https://es6.ruanyifeng.com/
+- http://jsrun.pro/tutorial/dZKKp
+- https://github.com/cucygh/fe-material
+- https://github.com/cucygh/es6-lessons
+- https://github.com/cucygh/es6-webpack
+- https://github.com/cucygh/ES6
+- http://es6-features.org/
+
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+let path = require('path');
+let webpack = require('webpack');
+module.exports = {
+    entry: './src/index.js',
+    mode:"development",
+    output: {
+        path: __dirname,
+        filename: './build/bundle.js'
+    },
+  
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            exclude: /(node_modules)/,
+            use:{
+                loader:"babel-loader",
+                options:{
+                    presets:[
+                        "env"
+                    ]
+                }
+            },
+        }]
+    },
+    devServer: {
+        disableHostCheck: true,
+        inline: true,
+        port: 8181
+    },
+}
